a02f57a623621c42e3f644e039125eb1c809fb9f
Akshatpatel204 <125722931+Akshatpatel204@users.noreply.github.com>
2026-02-04 18:27:55 +0530

    Update Compare.jsx

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -5,7 +5,7 @@ import Footer from '../component/Footer';
 const Compare = () => {
     return (
         <section className="overflow-hidden bg-black text-white bg-[url('https://lh3.googleusercontent.com/aida-public/AB6AXuCg-e_1wsAbpGbL7XlkBAWDJwHh8ihp8mEEsa1LLLwpcDeHLI3NpQ0ZINIRG1ht15OmuAz1d2XF0zr8IUS787GrpZiWem2tcAcih06zq9XkC-gvxvZerCcWOO2LZl9x3droSHEXZOkbEGdVDugjzXMTbX03Wr0VwA0H6AIMLS-tcnvLaRGsWXJ7QKkkvfo_m7_EFRv4wHe5vlBAOJwPVjqgtvSCpIy0ZXnkEme7Xn2bLP3DAtNybeWir4RCc5CXI3iIXbaC6Sn45s_i')] bg-cover bg-center">
-            <div className="flex items-end justify-between mb-8 px-4">
+            <div className="flex items-end justify-between mb-8 px-4 mt-4">
                 <div>
                     <h2 className="text-4xl font-black italic tracking-tighter uppercase" >
                         Battle of the <span className="text-primary">Machines</span>
@@ -31,3 +31,4 @@ const Compare = () => {
 }
 
 export default Compare
+
